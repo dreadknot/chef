33202d3e9065e90fbeabce2ff07ac4a755b00aeb
Mike Regan <dreadknot@gmail.com>
2013-12-10 14:24:49 -0800

    add log_dest var

--- cookbooks/logger/recipes/default.rb
+++ cookbooks/logger/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 template "/etc/rsyslog.d/25-logger.conf" do
-	variables :inputs_count => 200
+	variables :inputs_count => 200, :log_dest => "192.168.33.49"
 	source "logger.erb"
 	owner "root"
 	group "root"
